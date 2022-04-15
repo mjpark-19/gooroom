d84940d7834eb602344e13da461779b14fc450c3
mjpark <msyj10@gmail.com>
2022-04-15 17:17:53 +0900

    카카오 로그인 테스트

--- src/main/java/com/angkeum/gooroom/login/controller/OAuthController.java
+++ src/main/java/com/angkeum/gooroom/login/controller/OAuthController.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 @RestController
 @AllArgsConstructor
@@ -16,16 +20,33 @@ public class OAuthController {
     OAuthService oAuthService;
 
     private static final Logger log = LoggerFactory.getLogger(OAuthController.class);
+
+
     /**
      * 카카오 callback
      *[GET] login/oauth2/kakao
      * **/
     @ResponseBody
     @GetMapping("/kakao")
-    public void kakaoCallback(@RequestParam String code){
+    public void kakaoCallback(@RequestParam String code, HttpSession session){
         String accessToken = oAuthService.getKakaoToken(code);
         log.info(code);
-        oAuthService.createKakaoUser(accessToken);
+        HashMap<String, Object> userInfo= oAuthService.createKakaoUser(accessToken);
+        log.info(String.valueOf(userInfo));
+
+        if(userInfo.get("email") != null){
+         session.setAttribute("userId", userInfo.get("email"));
+         session.setAttribute("access_Token", accessToken);
+        }
     }
 
+
+
+    @RequestMapping("/logout")
+    public String logout(HttpSession session){
+        oAuthService.kakaoLogout((String)session.getAttribute("access_Token"));
+        session.removeAttribute("access_Token");
+        session.removeAttribute("userId");
+        return "index";
+    }
 }
--- src/main/java/com/angkeum/gooroom/login/service/OAuthService.java
+++ src/main/java/com/angkeum/gooroom/login/service/OAuthService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 
@@ -41,11 +42,11 @@ public class OAuthService {
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
             //TODO REST_API_KEY 입력
-            sb.append("&client_id=bd242b4183b0c00d04e0c5a336dfae33");
+            sb.append("&client_id=634e4c6f7e0c4f07491d5c9958590174");
             //TODO 인가코드 받은 redirect_uri 입력
             sb.append("&redirect_uri=http://localhost:8080/login/oauth2/kakao");
             sb.append("&code="+code);
-            sb.append("&client_secret=R8cXfVvrJWtLKK89aw8eIc2CWBtj3vDo");
+            //sb.append("&client_secret=R8cXfVvrJWtLKK89aw8eIc2CWBtj3vDo");
             System.out.println("스트링빌더: "+sb.toString());
             bw.write(sb.toString());
             bw.flush();
@@ -89,9 +90,11 @@ public class OAuthService {
         return accessToken;
     }
 
-    public void createKakaoUser(String token) {
+    public HashMap<String, Object> createKakaoUser(String token) {
+        HashMap<String, Object> userInfo = new HashMap<>();
         System.out.println("token : " + token);
-
+        String Bearer = "Bearer " + token;
+        System.out.println("Bearer : " + Bearer);
         String reqURL = "https://kapi.kakao.com/v2/user/me";
 
         //access_token을 이용하여 사용자 정보 조회
@@ -101,7 +104,7 @@ public class OAuthService {
 
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
-            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+            conn.setRequestProperty("Authorization", Bearer); //전송할 header 작성, access_token전송
 
             //결과 코드가 200이라면 성공
             int responseCode = conn.getResponseCode();
@@ -119,25 +122,70 @@ public class OAuthService {
             }
             System.out.println("response body : " + result);
 
+            /*프로필(profile)- json
+             String - email, age_range, birthday, birthyear, gender, phone_number*/
+
             //Gson 라이브러리로 JSON파싱
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
 
             int id = element.getAsJsonObject().get("id").getAsInt();
             boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
+            boolean hasAgeRange = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_age_range").getAsBoolean();
+            boolean hasBirthday = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_birthday").getAsBoolean();
+            boolean hasGender = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_gender").getAsBoolean();
+
             String email = "";
+            String age = "";
+            String birthday = "";
+            String gender = "";
             if(hasEmail){
                 email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
             }
 
+               /* age = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("age").getAsString();
+                birthday = element.getAsJsonObject().get("properties").getAsJsonObject().get("birthday").getAsString();
+                gender = element.getAsJsonObject().get("properties").getAsJsonObject().get("gender").getAsString();*/
+
             System.out.println("id : " + id);
             System.out.println("email : " + email);
+            System.out.println("ageRange : " + age);
+            System.out.println("birthday : " + birthday);
+            System.out.println("gender : " + gender);
+
+            userInfo.put("id", id);
+            userInfo.put("email", email);
 
             br.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return userInfo;
     }
 
+    public void kakaoLogout(String token){
+        String reqUrl = "https://kapi.kakao.com/v1/user/logout";
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization","Bearer "+ token);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String result = "";
+            String line = "";
+
+            while((line = br.readLine())!= null){
+                result += line;
+            }
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
